a92be6854e1675056eea9d28055a978e3eaed30e
Romualdo Beltrán Félix <robbil8bit@gmail.com>
2019-10-29 15:03:51 -0700

    Backend Login y Registro

---
+++ database.php
@@ -0,0 +1,15 @@
+<?php
+
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'php_login_database';
+
+try {
+  $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
+} catch (PDOException $e) {
+  die('Connection Failed: ' . $e->getMessage());
+}
+
+?>
+
