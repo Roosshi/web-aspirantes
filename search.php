3bdff273792b7fe29f892d625267f44d5591fa14
Martin Hurtado <martinhurtadoarellanolol@hotmail.com>
2019-12-04 21:04:46 -0700

    Vista del gestor de busqueda de aspirantes lista para ser probada.

--- search.php
+++ search.php
@@ -17,21 +17,30 @@
 
 		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
-		<link rel="stylesheet" href="assets/css/style.css">
+		<link rel="stylesheet" href="assets/css/style-search.css">
 	</head>
 
 	<body>
 		<?php require 'Menu.html' ?>
 
 		<?php
-			// if($user['es_admin']):
-			if($_SESSION['user_id']):
+			//if($_SESSION['id_admin']):
+			if(true):
 		?>
 
-			<h1>Buscar aspirantes</h1>
-			<span>Despues de terminar, descomenta el otro if de php.</span>
 
 			<div class="container">
+				<div class="row">
+					<div class="col"><h1>Buscar aspirantes</h1></div>
+					<div class="col">
+						<div class="input-group mb-3">
+							<input type="text" id="nombre" class="form-control" placeholder="Nombre del aspirante">
+							<div class="input-group-append">
+								<button class="btn btn-outline-secondary" onclick="buscarAspirante(nombre.value);" type="button">Buscar Aspirante</button>
+							</div>
+						</div>
+					</div>
+				</div>
 				<table class="table">
 					<thead>
 						<tr>
@@ -62,5 +71,7 @@
 			header("Location: /web-aspirantes")
 		?>
 		<?php endif; ?>
+
+		<script src="./assets/js/search.js"></script>
 	</body>
 </html>
